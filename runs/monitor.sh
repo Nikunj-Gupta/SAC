60e1c56118267e912bd6bfa15d5cdaf5a4dfa97f
Nikunj-Gupta <Nikunj.Gupta@iiitb.org>
2021-07-30 12:41:27 +0530

    updated to def-mtaylor3

--- runs/monitor.sh
+++ runs/monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=rrg-mtaylor3
+#SBATCH --account=def-mtaylor3
 #SBATCH --cpus-per-task=1   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=3200M        # memory per node
 #SBATCH --time=0-0:15      # time (DD-HH:MM)
